3c6aea706b7937acc7dccf8cbbe8e58ee97b5279
Cloud <cloud@txthinking.com>
2015-11-16 06:27:31 +0000

    vim + py dev

--- vim/use.sh
+++ vim/use.sh
@@ -5,5 +5,6 @@ rm -rf ~/.vim
 mkdir ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+sudo apt-get -y install python-dev
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh
