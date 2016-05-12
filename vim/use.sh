c84617b957c51ab29c4ce11ead58cc8fe381a98a
Cloud <cloud@txthinking.com>
2016-05-12 12:46:40 +0000

    vim-nox

--- vim/use.sh
+++ vim/use.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+sudo apt-get -y install make gcc g++ python-dev cmake vim-nox
+
 cp .vimrc ~
 rm -rf ~/.vim
 mkdir ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 vim +GoInstallBinaries +qall
-sudo apt-get -y install python-dev cmake
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 ./install.py
